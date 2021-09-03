be8a2c6216d31b9437cd28e99dda77aa86f3872e
Dom McCann <dommccann@Doms-MacBook-Pro.local>
2021-09-03 16:25:17 +1200

    further work on projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,33 @@
 import "./Projects.css";
 import { LeftBar } from "../components/LeftBar";
 
+const projectData = [
+  {
+    name: "Optinum",
+    description:
+      "A mobile app that colates Everton Football club forums into one easy to read space.",
+    technologies: ["Puppeteer", "Javascript", "Express", "React Native"],
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fbusiness-56635888&psig=AOvVaw3pg6L25X2xAy1YCUpmtlma&ust=1630729479130000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJiOu-764fICFQAAAAAdAAAAABAD",
+  },
+];
+
 function Projects() {
   return (
     <div className="container">
       <LeftBar />
       <div className="main">
         <div className="center-space">
-          <p className="project-title">PROJECTS</p>
+          <div className="wrapper">
+            <p className="project-title">PROJECTS</p>
+            <p className="project-title-subtext">
+              I like to create applications that improve my day to day life.
+              Below you can find a list of all my project including those that
+              are still a work in progress. Further details can be found inside
+              the project page including the github link and other relavant
+              infomation.
+            </p>
+            <div className="project-space"></div>
+          </div>
         </div>
       </div>
     </div>
