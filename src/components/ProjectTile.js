bef7fa700fa60e566c08212dcb27fe2905908d16
Dom McCann <dommccann@Doms-MacBook-Pro.local>
2021-09-20 07:38:41 +1200

    fixed image sizing

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -1,10 +1,18 @@
 import "./ProjectTile.css";
+import cover from "../res/mobile-app-cover.png";
+import { co } from "co";
 
 export const ProjectTile = ({ data }) => {
   return (
     <div className="project-container">
-      <h1>{data.name}</h1>
-      <a>{data.description}</a>
+      <div className="project-image-space">
+        <img src={cover} className="cover-image" />
+      </div>
+      <div className="project-infomation">
+        <h1>{data.name}</h1>
+        <a>{data.description}</a>
+        <a>{data.technologies}</a>
+      </div>
     </div>
   );
 };
